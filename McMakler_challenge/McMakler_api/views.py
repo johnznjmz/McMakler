9f4f3af8c6018d10b1288c6372b49228e155a588
johnson james <38066495+johnznjmz@users.noreply.github.com>
2021-09-15 09:40:59 +0200

    Update views.py

--- McMakler_challenge/McMakler_api/views.py
+++ McMakler_challenge/McMakler_api/views.py
@@ -12,17 +12,19 @@ class McMacklerView(views.APIView):
 
     def get(self, request):
 
-        field = request.query_params.get('field', None).lower()  # GET column name from pram feild
+        field = request.query_params.get('field', None)  # GET column name from pram feild
         method = request.query_params.get('method', None)  # GET choice of methods (value or common) from pram method
 
         while field:
-            queryset = Mcmackler.objects.values(field).order_by('id')  # create queryset based on column name in variable field
+            column = field.lower()  # Chnage paramter value into lower case
+            queryset = Mcmackler.objects.values(column).order_by('id')  # create queryset based on column name in variable field
+            
             if method == 'values':
                 result = queryset.distinct().count()  # count distcint values in the queryset
                 break
 
             elif method == 'common':
-                frequent_value = queryset.annotate(freq_value=Count(field)).order_by('-freq_value')[0]  # caluclate most frequent value in the queryset
+                frequent_value = queryset.annotate(freq_value=Count(column)).order_by('-freq_value')[0]  # caluclate most frequent value in the queryset
                 result = list(frequent_value.values())[0]  # pass frequent value to variable result
                 break
 
