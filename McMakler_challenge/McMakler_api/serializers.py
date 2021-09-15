a442bae6ecba20bc8399699b60bbeeef0eae4787
johnson james <38066495+johnznjmz@users.noreply.github.com>
2021-09-15 02:12:37 +0200

    Update serializers.py

--- McMakler_challenge/McMakler_api/serializers.py
+++ McMakler_challenge/McMakler_api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 
+"""data serializer, define fields here."""
 
-class McMacklerSerializer(serializers.Serializer):
-    """data serializer, define fields here."""
+class McMacklerCharSerializer(serializers.Serializer):
+
     result = serializers.CharField()
+
+
+class McMacklerIntSerializer(serializers.Serializer):
+
+    result = serializers.IntegerField()
 
